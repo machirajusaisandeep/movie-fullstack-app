65b5e9c9d5996382e6539fecc40c66942ead9283
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2025-10-22 11:40:40 +0530

    added logger to project

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fullstack/movie/logger"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Initialize logger
+	logInstance := initializeLogger()
 
-	http.Handle("/", http.FileServer(http.Dir("public"))) // by default - relative path is used here
+	http.Handle("/", http.FileServer(http.Dir("public")))
 
+	// Start server
 	const addr = ":8080"
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		// not working - error
-		log.Fatalf("Server failed %v", err)
+	logInstance.Info("Server starting on " + addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		logInstance.Error("Server failed to start", err)
+		log.Fatalf("Server failed: %v", err)
 	}
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	return logInstance
 }
